6636077e65a332e0302cdaf7a2a098a99d9cb42b
Fernanjacome <fernanjacome02@gmail.com>
2024-12-07 21:23:38 -0500

    Viewport

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -22,7 +22,7 @@ class MyDocument extends Document {
           <meta charSet="UTF-8" />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1.0"
+            content="width=device-width, initial-scale=0.70, maximum-scale=0.70, user-scalable=no"
           />
           <meta
             name="description"
